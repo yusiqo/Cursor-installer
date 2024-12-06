f560f3fa8e09c673604ca903a5a1a0be72429966
yusiqo <80514373+yusiqo@users.noreply.github.com>
2024-12-07 00:29:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# AppImage URL'si, ikon URL'si ve uygulama adı
 APP_URL="https://download.todesktop.com/230313mzl4w4u92/cursor-0.43.6-build-241206z7j6me2e2-x86_64.AppImage"
 ICON_URL="https://raw.githubusercontent.com/yusiqo/Cursor-installer/refs/heads/main/cursor.png"
 APP_NAME="Cursor"
 INSTALL_DIR="/opt/$APP_NAME"
-sudo mkdir -p "$INSTALL_DIR"
-echo "AppImage Downloading..."
-curl -L "$APP_URL" -o "$INSTALL_DIR/$APP_NAME.AppImage"
 
+# Create the necessary directory
+sudo mkdir -p "$INSTALL_DIR"
+
+# Download AppImage
+echo "AppImage Downloading..."
+sudo curl -L "$APP_URL" -o "$INSTALL_DIR/$APP_NAME.AppImage"
+
+# Make it executable
 sudo chmod +x "$INSTALL_DIR/$APP_NAME.AppImage"
 
+# Download icon
 echo "İcon Downloading..."
-curl -L "$ICON_URL" -o "$INSTALL_DIR/$APP_NAME.png"
+sudo curl -L "$ICON_URL" -o "$INSTALL_DIR/$APP_NAME.png"
 
+# Create desktop entry
 DESKTOP_FILE="/usr/share/applications/$APP_NAME.desktop"
-
 echo "Desktop file creating..."
 sudo bash -c "cat > $DESKTOP_FILE" <<EOL
 [Desktop Entry]
@@ -27,5 +32,4 @@ Categories=Utility;
 Terminal=false
 EOL
 
-# Başarılı mesajı
-echo "$APP_NAME Succesfuly, icon added and app installed."
+echo "$APP_NAME Successfully installed with icon and added to app list."
